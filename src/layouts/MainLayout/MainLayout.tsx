2e9c39f2842d6f1d012b8a1227f56e51cd5a5ed5
Safeya-Yasien <ssafeya5@gmail.com>
2024-12-31 14:05:43 +0200

    create main layout

---
+++ src/layouts/MainLayout/MainLayout.tsx
@@ -0,0 +1,12 @@
+import { Navbar } from "@/components/shared"
+
+const MainLayout = () => {
+  return (
+    <div className="font-jakarta h-screen overflow-x-hidden">
+      <Navbar/>
+      
+    </div>
+  )
+}
+export default MainLayout
+
