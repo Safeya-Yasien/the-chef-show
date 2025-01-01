197ced7f81e2c09cb8a16c04389b10177c98301d
Safeya-Yasien <ssafeya5@gmail.com>
2025-01-01 16:09:38 +0200

    update MainLayout.tsx

--- src/layouts/MainLayout/MainLayout.tsx
+++ src/layouts/MainLayout/MainLayout.tsx
@@ -2,13 +2,13 @@ import { Navbar, Footer } from "@/components/shared";
 import { Outlet } from "react-router-dom";
 
 const MainLayout = () => {
-  console.log("MainLayout rendered");
-
   return (
-    <div className="font-jakarta h-screen overflow-x-hidden">
+    <div className="font-jakarta h-screen overflow-x-hidden [background:radial-gradient(42.7%_74.11%_at_50%_0%,_#1D1D1D_0%,_#131313_100%)]">
       <Navbar />
-      <Outlet />
-      <Footer/>
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
     </div>
   );
 };
