6f37a76a3bf039489fc2b20213f0b0e9ae81f025
Safeya-Yasien <ssafeya5@gmail.com>
2025-01-04 15:34:05 +0200

    update src/components/index.ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,8 @@
 import Hero from "./hero/Hero";
+import About from "./about/About";
+import Experience from "./experience/Experience";
+import SpecialMenu from "./specialMenu/SpecialMenu";
+import Testimonials from "./testimonials/Testimonials";
+import Support from "./support/Support";
 
-export { Hero };
+export { Hero, About, Experience, SpecialMenu, Testimonials, Support };
