fec859bc6bc41f7aea6dafc11af081a279b4d63f
Safeya-Yasien <ssafeya5@gmail.com>
2025-01-06 09:40:28 +0200

    update Contact.tsx

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -15,7 +15,7 @@ const Contact = () => {
       {/* hero */}
       <div
         className="rounded-br-[120px] rounded-bl-[120px] border-b border-[#C19F74] flex flex-col items-center justify-center h-[420px] text-center 
-   bg-no-repeat bg-center"
+                  bg-no-repeat bg-center"
         style={{ backgroundImage: `url(${group})` }}
       >
         <div className="relative ">
@@ -50,66 +50,69 @@ const Contact = () => {
 
       {/* form */}
       <div className="px-[80px] ">
-      <div className="grid grid-cols-1 md:grid-cols-2 border border-[#C19F74] mt-[80px] ">
-        <div className="py-[64px] px-[40px]">
-          <h2 className="font-restora text-white font-normal text-[48px] tracking-[0.02em] capitalize mb-[24px]">
-            Contact us
-          </h2>
-          <p className="text-white font-thin text-[20px] leading-[32px] mb-10">
-            Hello! Do you have any questions or suggestions about this site, or
-            just want to say Hi? Send a message using the below form. I will get
-            back to you as soon as possible.
-          </p>
+        <div className="grid grid-cols-1 md:grid-cols-2 border border-[#C19F74] mt-[80px] ">
+          <div className="py-[64px] px-[40px]">
+            <h2 className="font-restora text-white font-normal text-[48px] tracking-[0.02em] capitalize mb-[24px]">
+              Contact us
+            </h2>
+            <p className="text-white font-thin text-[20px] leading-[32px] mb-10">
+              Hello! Do you have any questions or suggestions about this site,
+              or just want to say Hi? Send a message using the below form. I
+              will get back to you as soon as possible.
+            </p>
 
-          <form className="flex flex-col gap-4">
-            {/* Email Field */}
-            <div className="flex flex-col">
-              <label className="text-white font-thin text-[16px]">Email</label>
-              <input
-                type="email"
-                className="border border-[#C19F74] p-[10px] mt-2 bg-transparent text-white placeholder-gray-400 outline-none"
-                placeholder="Enter Your Email"
-              />
-            </div>
+            <form className="flex flex-col gap-4">
+              {/* Email Field */}
+              <div className="flex flex-col">
+                <label className="text-white font-thin text-[16px]">
+                  Email
+                </label>
+                <input
+                  type="email"
+                  className="border border-[#C19F74] p-[10px] mt-2 bg-transparent text-white placeholder-gray-400 outline-none"
+                  placeholder="Enter Your Email"
+                />
+              </div>
 
-            {/* Subject Field */}
-            <div className="flex flex-col">
-              <label className="text-white font-thin text-[16px]">
-                Subject
-              </label>
-              <input
-                type="text"
-                className="border border-[#C19F74] p-[10px] mt-2 bg-transparent text-white placeholder-gray-400 outline-none"
-                placeholder="Enter Your Subject"
-              />
-            </div>
+              {/* Subject Field */}
+              <div className="flex flex-col">
+                <label className="text-white font-thin text-[16px]">
+                  Subject
+                </label>
+                <input
+                  type="text"
+                  className="border border-[#C19F74] p-[10px] mt-2 bg-transparent text-white placeholder-gray-400 outline-none"
+                  placeholder="Enter Your Subject"
+                />
+              </div>
 
-            {/* Message Field */}
-            <div className="flex flex-col">
-              <label className="text-white font-thin text-[16px]">
-                Message
-              </label>
-              <textarea
-                className="border border-[#C19F74] h-[120px] p-[10px] mt-2 bg-transparent text-white placeholder-gray-400 outline-none resize-none"
-                placeholder="Enter Your Message"
-              />
-            </div>
+              {/* Message Field */}
+              <div className="flex flex-col">
+                <label className="text-white font-thin text-[16px]">
+                  Message
+                </label>
+                <textarea
+                  className="border border-[#C19F74] h-[120px] p-[10px] mt-2 bg-transparent text-white placeholder-gray-400 outline-none resize-none"
+                  placeholder="Enter Your Message"
+                />
+              </div>
 
-            <button className="bg-[#C19F74] text-white w-auto md:w-[256px] h-[52px] flex items-center justify-center uppercase font-medium text-sm tracking-[0.04em] mt-[32px] ">
-              send a message
-            </button>
-          </form>
+              <button className="bg-[#C19F74] text-white w-auto md:w-[256px] h-[52px] flex items-center justify-center uppercase font-medium text-sm tracking-[0.04em] mt-[32px] ">
+                send a message
+              </button>
+            </form>
+          </div>
+
+          {/* img */}
+          <div className="hidden md:block">
+            <img
+              src={contact}
+              alt="contact"
+              loading="lazy"
+              className="h-full w-full "
+            />
+          </div>
         </div>
-
-        {/* img */}
-        <div className="hidden md:block">
-          <img
-            src={contact}
-            alt="contact"
-            loading="lazy"
-            className="h-full w-full "
-          />
-        </div></div>
       </div>
     </div>
   );
