19f54b7a69f13ebe9ebca6bfa791ef4570c24782
Safeya-Yasien <ssafeya5@gmail.com>
2025-01-14 23:46:36 +0200

    update ScrollToTop.tsx by using useLayoutEffect instead of useEffect

--- src/components/shared/scrollToTop/ScrollToTop.tsx
+++ src/components/shared/scrollToTop/ScrollToTop.tsx
@@ -1,16 +1,12 @@
-import { useEffect } from "react";
+import { useLayoutEffect } from "react";
 import { useLocation } from "react-router-dom";
 
 const ScrollToTop = () => {
-  const { pathname } = useLocation();
+  const location = useLocation();
 
-  useEffect(() => {
-    console.log("Pathname changed:", pathname);
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth",
-    });
-  }, [pathname]);
+  useLayoutEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location.pathname]);
 
   return null;
 };
