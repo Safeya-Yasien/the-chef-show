c98c5906f51676d4f9d56aa96bf217ec344e5776
Safeya-Yasien <ssafeya5@gmail.com>
2025-01-22 22:57:54 +0200

    update CustomTitle.tsx

--- src/components/shared/customTitle/CustomTitle.tsx
+++ src/components/shared/customTitle/CustomTitle.tsx
@@ -15,7 +15,7 @@ const CustomTitle = ({ imgSrc, subTitle, mainTitle }: ICustomTitleProps) => {
         <span className="absolute -left-4 top-1/2 -translate-y-1/2 w-[6px] h-[6px] rotate-45 bg-[#C19F74]"></span>
         <span className="absolute -right-4 top-1/2 -translate-y-1/2 w-[6px] h-[6px] rotate-45 bg-[#C19F74]"></span>
       </div>
-      <h1 className="font-normal text-[28px] md:text-[36px] lg:text-[48px] leading-7 md:leading-10 lg:leading-[56px] text-white font-restora uppercase">
+      <h1 className="text-center font-normal text-[28px] md:text-[36px] lg:text-[48px] leading-7 md:leading-10 lg:leading-[56px] text-white font-restora uppercase">
         {mainTitle}
       </h1>
     </div>
