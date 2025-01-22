46e35f8a244e23206b4e7004c6f36a6b508b3920
Safeya-Yasien <ssafeya5@gmail.com>
2025-01-22 19:33:01 +0200

    build(deps): update package-lock.json, package.json, Hero.tsx and Navbar.tsx update menu as sheet

--- src/components/shared/navbar/Navbar.tsx
+++ src/components/shared/navbar/Navbar.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 
+import SheetMenu from "./SheetMenu";
+
 const navLinks = [
   { to: "/", label: "Home" },
   { to: "/reservation", label: "Reservation" },
@@ -22,13 +24,13 @@ const NavLink: React.FC<INavLinkProps> = ({ to, children, closeMenu }) => {
     <Link
       to={to}
       onClick={closeMenu}
-      className={`uppercase text-white font-normal text-xl transition-colors h-full flex items-center relative group`}
+      className={`uppercase text-white font-normal text-lg transition-colors h-full flex items-center relative group`}
       aria-label={`Navigate to ${children}`}
     >
       {children}
       {/* Hover and active border with rounded corners */}
       <span
-        className={`absolute bottom-0 left-0 w-full h-[5px] bg-[#C19F74] transform scale-x-0 transition-transform group-hover:scale-x-100 ${
+        className={` absolute bottom-0 left-0 w-full h-[5px] bg-[#C19F74] transform scale-x-0 transition-transform group-hover:scale-x-100 ${
           isActive ? "scale-x-100" : ""
         } rounded-md`}
       ></span>
@@ -74,7 +76,7 @@ export function Navbar() {
     } `}
     >
       <nav className="w-full h-full z-20 top-0 start-0 px-4 xl:px-[60px]">
-        <div className="flex flex-wrap items-center justify-between mx-auto px-4 h-full">
+        <div className="flex items-center justify-between mx-auto px-4 h-full">
           {/* logo */}
           <Link
             to="/"
@@ -88,51 +90,20 @@ export function Navbar() {
             />
           </Link>
 
-          <div className="flex items-center xl:order-2 space-x-3 xl:space-x-0 rtl:space-x-reverse h-full">
-            <Link
-              to="/reservation"
-              className="text-white bg-[#C19F74] w-[150px] h-[50px] shadow-[4px_4px_10px_0px_#00000014] text-sm font-medium leading-[0.04em]  items-center justify-center hover:bg-[#A88A62] transition-colors hidden sm:flex"
-              aria-label="Go to the reservation page"
-            >
-              RESERVATION
-            </Link>
-            <button
-              type="button"
-              className="inline-flex items-center p-2 w-11 h-11 justify-center text-sm text-gray-500 xl:hidden hover:text-[#C19F74] focus:outline-none"
-              aria-controls="navbar-sticky"
-              aria-expanded={isMenuOpen}
-              onClick={() => setIsMenuOpen(!isMenuOpen)}
-            >
-              <span className="sr-only">
-                {isMenuOpen ? "Close main menu" : "Open main menu"}
-              </span>
-              <svg
-                className="w-7 h-7"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 17 14"
-              >
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M1 1h15M1 7h15M1 13h15"
-                />
-              </svg>
-            </button>
+          <div className="flex items-center    h-full">
+            {/* Mobile Menu Button (Opens Drawer) */}
+            <SheetMenu navLinks={navLinks} />
           </div>
           <div
             className={`items-center justify-between h-full ${
               isMenuOpen ? "block" : "hidden"
-            } w-full xl:flex xl:w-auto xl:order-1 transition-all duration-300 ease-in-out`}
+            } lg:flex w-auto  transition-all duration-300 ease-in-out`}
             id="navbar-sticky"
           >
             {/* links */}
-            <ul className="flex flex-col xl:flex-row gap-4 absolute top-[88px] xl:top-auto left-0 xl:left-auto p-6 xl:p-0 bg-[#232323] xl:bg-transparent xl:relative xl:gap-[64px] items-center w-full xl:w-auto xl:h-full">
+            <ul className="flex bg-transparent xl:relative gap-[64px] items-center w-full w-auto h-full">
               {navLinks.map((link) => (
-                <li key={link.to} className="w-full xl:w-auto h-full">
+                <li key={link.to} className="w-auto h-full">
                   <NavLink to={link.to} closeMenu={closeMenu}>
                     {link.label}
                   </NavLink>
@@ -140,6 +111,13 @@ export function Navbar() {
               ))}
             </ul>
           </div>
+          <Link
+            to="/reservation"
+            className="text-white bg-[#C19F74] w-[150px] h-[50px] shadow-[4px_4px_10px_0px_#00000014] text-sm font-medium leading-[0.04em]  items-center justify-center hover:bg-[#A88A62] transition-colors hidden lg:flex"
+            aria-label="Go to the reservation page"
+          >
+            RESERVATION
+          </Link>
         </div>
       </nav>
     </header>
---
+++ src/components/shared/navbar/SheetMenu.tsx
@@ -0,0 +1,54 @@
+import { Link, useLocation } from "react-router-dom";
+
+import { Button } from "@/components/ui/button";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetFooter,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { Menu } from "lucide-react";
+
+interface INavLinkProps {
+  to: string;
+  label: string;
+}
+
+const SheetMenu = ({ navLinks }: { navLinks: INavLinkProps[] }) => {
+  const location = useLocation();
+
+  return (
+    <Sheet>
+      <SheetTrigger asChild className="">
+        <Button className="text-lg bg-transparent flex justify-center p-1 items-center lg:hidden hover:bg-white hover:text-black rounded-none">
+          <Menu className="!w-8 !h-8" />
+        </Button>
+      </SheetTrigger>
+      <SheetContent>
+        <nav className="flex flex-col space-y-4 mt-8">
+          {navLinks.map((link: INavLinkProps) => {
+            const isActive = location.pathname === link.to;
+
+            return (
+              <Link
+                key={link.to}
+                to={link.to}
+                className={`capitalize flex w-full items-center py-2 text-lg font-bold italic font-restora transition-colors ${
+                  isActive ? "text-[#C19F74]" : "text-white"
+                }`}
+              >
+                {link.label}
+              </Link>
+            );
+          })}
+        </nav>
+        <SheetFooter>
+          <SheetClose asChild className=""></SheetClose>
+        </SheetFooter>
+      </SheetContent>
+    </Sheet>
+  );
+};
+
+export default SheetMenu;
