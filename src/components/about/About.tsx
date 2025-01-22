f883e1ec0ecde405835a14a5324c39f7d3502922
Safeya-Yasien <ssafeya5@gmail.com>
2025-01-23 00:26:22 +0200

    update About.tsx

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -10,8 +10,12 @@ const About = () => {
       <div className="container px-4 mx-auto">
         {/* custom title  */}
         <div className="flex flex-col items-center justify-center text-center mb-20 gap-3">
-          <CustomTitle subTitle="All Day Experience" imgSrc={forkKnife} mainTitle=" About The chef show"/>
-        
+          <CustomTitle
+            subTitle="All Day Experience"
+            imgSrc={forkKnife}
+            mainTitle=" About The chef show"
+          />
+
           <p className="font-normal text-sm md:text-[16px] text-white w-auto md:w-[668px] capitalize leading-5 md:leading-7">
             Our restaurant offers a stylish and fun European cafe-restaurant
             experience, inspired by Executive Cheft Chris Hill, the all day
@@ -35,7 +39,7 @@ const About = () => {
               <h2 className="font-normal text-[48px] text-[#C19F74] tracking-[0.02em] leading-[48px] ">
                 490+{" "}
               </h2>
-              <h3 className="text-white text-[40px] font-normal tracking-[0.02em] leading-[40px] font-restora capitalize">
+              <h3 className="text-white text-[30px] md:text-[40px] font-normal tracking-[0.02em] leading-[40px] font-restora capitalize">
                 positive feedback
               </h3>
               <p className="text-white font-normal text-[16px] leading-[24px]">
@@ -58,7 +62,7 @@ const About = () => {
               <h2 className="font-normal text-[48px] text-[#C19F74] tracking-[0.02em] leading-[48px] ">
                 1590+{" "}
               </h2>
-              <h3 className="text-white text-[40px] font-normal tracking-[0.02em] leading-[40px] font-restora capitalize">
+              <h3 className="text-white text-[30px] md:text-[40px] font-normal tracking-[0.02em] leading-[40px] font-restora capitalize">
                 delivery order{" "}
               </h3>
               <p className="text-white font-normal text-[16px] leading-[24px]">
@@ -81,7 +85,7 @@ const About = () => {
               <h2 className="font-normal text-[48px] text-[#C19F74] tracking-[0.02em] leading-[48px] ">
                 100%
               </h2>
-              <h3 className="text-white text-[40px] font-normal tracking-[0.02em] leading-[40px] font-restora capitalize">
+              <h3 className="text-white text-[30px] md:text-[40px] font-normal tracking-[0.02em] leading-[40px] font-restora capitalize">
                 visitor Daily{" "}
               </h3>
               <p className="text-white font-normal text-[16px] leading-[24px]">
