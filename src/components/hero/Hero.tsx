4b94ca9f6e5a56c3532f06d83eef5b8b6a2d7185
Safeya-Yasien <ssafeya5@gmail.com>
2025-01-22 17:22:58 +0200

    rename 1 file, create 1 file and update 1 file

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,4 +1,5 @@
-import hero from "../../assets/images/hero.jpg";
+import carousel1 from "../../assets/images/carousel1.jpg";
+import carousel2 from "../../assets/images/carousel2.webp";
 import barbecueSteak from "../../assets/images/barbecueSteak.svg";
 
 import { CustomTitle } from "../shared";
@@ -8,7 +9,7 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Hero = () => {
-  const bgImages = [hero, hero, hero];
+  const bgImages = [carousel1, carousel2, carousel1];
   const [currentIndex, setCurrentIndex] = useState(0);
 
   const handlePrev = () => {
@@ -25,12 +26,12 @@ const Hero = () => {
 
   return (
     <div
-      className="hero-section bg-no-repeat bg-cover bg-center rounded-br-[120px] rounded-bl-[120px]  h-screen
+      className="hero-section bg-no-repeat bg-cover bg-center rounded-br-[60px] rounded-bl-[60px] md:rounded-br-[120px] md:rounded-bl-[120px  h-[90vh]
   flex flex-col items-center justify-center relative"
       style={{ backgroundImage: `url(${bgImages[currentIndex]})` }}
     >
       {/* overlay */}
-      <div className="absolute w-full h-full top-0 left-0 bg-[#000000BF] rounded-br-[120px] rounded-bl-[120px] border-b-[1px] border-b-[#C19F74]"></div>
+      <div className="absolute w-full h-full top-0 left-0 bg-[#000000BF] rounded-br-[60px] rounded-bl-[60px] md:rounded-br-[120px] md:rounded-bl-[120px] border-b-[1px] border-b-[#C19F74]"></div>
 
       {/* text */}
       <div className="flex flex-col items-center justify-center gap-[15px] text-center  w-auto md:w-[800px] relative z-10">
@@ -55,7 +56,7 @@ const Hero = () => {
 
       {/* Navigation Arrows */}
 
-      <div className="absolute flex justify-between items-center w-full h-full px-10">
+      <div className="absolute hidden md:flex justify-between items-center w-full h-full px-10">
         <button
           className="text-white  border border-white  w-[80px] h-[80px] flex items-center justify-center rounded-full"
           onClick={handlePrev}
@@ -68,6 +69,19 @@ const Hero = () => {
         >
           <GoArrowRight className="w-[40px] h-[40px] " />
         </button>
+      </div>
+
+      {/* Indicators (Visible on Small Screens) */}
+      <div className="absolute bottom-5 flex gap-2 md:hidden">
+        {bgImages.map((_, index) => (
+          <button
+            key={index}
+            className={`w-3 h-3 rounded-full ${
+              currentIndex === index ? "bg-[#C19F74]" : "bg-gray-400"
+            }`}
+            onClick={() => setCurrentIndex(index)}
+          />
+        ))}
       </div>
     </div>
   );
