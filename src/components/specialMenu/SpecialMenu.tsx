161672c7db3afc30c6c78fb5476c17cf37d8c33e
Safeya-Yasien <ssafeya5@gmail.com>
2025-01-22 22:51:33 +0200

    update SpecialMenu.tsx

--- src/components/specialMenu/SpecialMenu.tsx
+++ src/components/specialMenu/SpecialMenu.tsx
@@ -49,24 +49,26 @@ const products = [
 const SpecialMenu = () => {
   return (
     <section className="pt-[120px] ">
-      <div className="flex flex-col items-center justify-center text-center mb-20 w-auto md:w-[800px] mx-auto">
-        <CustomTitle title="Special Offers" imgSrc={servingDome} />
-        <h1 className="font-normal text-[40px] md:text-[56px] tracking-[0.02em] text-white font-restora uppercase">
-          THE special menu
-        </h1>
-      </div>
+      <div className="container px-4 mx-auto">
+        <div className="flex flex-col items-center justify-center text-center mb-20 gap-3">
+          <CustomTitle title="Special Offers" imgSrc={servingDome} />
+          <h1 className="font-normal text-[28px] md:text-[36px] lg:text-[48px] leading-7 md:leading-10 lg:leading-[56px] text-white font-restora uppercase">
+            THE special menu
+          </h1>
+        </div>
 
-      {/* products */}
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 gap-y-[60px]">
-        {/* single product */}
-        {products.map((product, index) => (
-          <ProductCard
-            key={index}
-            imgSrc={product.imgSrc}
-            title={product.title}
-            description={product.description}
-          />
-        ))}
+        {/* products */}
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 gap-y-[60px]">
+          {/* single product */}
+          {products.map((product, index) => (
+            <ProductCard
+              key={index}
+              imgSrc={product.imgSrc}
+              title={product.title}
+              description={product.description}
+            />
+          ))}
+        </div>
       </div>
     </section>
   );
