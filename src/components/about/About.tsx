3212a836d56dfd78da83ecfe92bb683c0c3b842c
Safeya-Yasien <ssafeya5@gmail.com>
2025-01-22 22:51:40 +0200

    update About.tsx

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -7,88 +7,90 @@ import image2 from "../../assets/images/aboutImages/image2.jpg";
 const About = () => {
   return (
     <section className="pt-[120px]" id="contact">
-      {/* custom title  */}
-      <div className="flex flex-col items-center justify-center text-center mb-20">
-        <CustomTitle title="All Day Experience" imgSrc={forkKnife} />
-        <h1 className="font-normal text-[40px] md:text-[56px] tracking-[0.02em] text-white font-restora uppercase">
-          About The chef show
-        </h1>
-        <p className="font-normal text-[16px] text-white w-auto md:w-[668px] capitalize">
-          Our restaurant offers a stylish and fun European cafe-restaurant
-          experience, inspired by Executive Cheft Chris Hill, the all day
-          dinning menu is a reflection of the very best of the season.
-        </p>
-      </div>
+      <div className="container px-4 mx-auto">
+        {/* custom title  */}
+        <div className="flex flex-col items-center justify-center text-center mb-20 gap-3">
+          <CustomTitle title="All Day Experience" imgSrc={forkKnife} />
+          <h1 className="font-normal text-[28px] md:text-[36px] lg:text-[48px] leading-7 md:leading-10 lg:leading-[56px] text-white font-restora uppercase">
+            About The chef show
+          </h1>
+          <p className="font-normal text-sm md:text-[16px] text-white w-auto md:w-[668px] capitalize leading-5 md:leading-7">
+            Our restaurant offers a stylish and fun European cafe-restaurant
+            experience, inspired by Executive Cheft Chris Hill, the all day
+            dinning menu is a reflection of the very best of the season.
+          </p>
+        </div>
 
-      {/*  */}
-      <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-[48px]">
-        <div className="h-auto md:h-[714px] flex flex-col gap-[20px]">
-          <div className="w-auto md:w-[438px] h-[518px]">
-            <img
-              src={image}
-              alt=""
-              loading="lazy"
-              className="object-cover w-full h-full"
-            />
+        {/*  */}
+        <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-[48px]">
+          <div className="h-auto md:h-[714px] flex flex-col gap-[20px]">
+            <div className="w-auto md:w-[438px] h-[518px]">
+              <img
+                src={image}
+                alt=""
+                loading="lazy"
+                className="object-cover w-full h-full"
+              />
+            </div>
+            {/* text */}
+            <div className="flex flex-col gap-2">
+              <h2 className="font-normal text-[48px] text-[#C19F74] tracking-[0.02em] leading-[48px] ">
+                490+{" "}
+              </h2>
+              <h3 className="text-white text-[40px] font-normal tracking-[0.02em] leading-[40px] font-restora capitalize">
+                positive feedback
+              </h3>
+              <p className="text-white font-normal text-[16px] leading-[24px]">
+                Our restaurant offers a stylish and fun European cafe-restaurant
+                experience.
+              </p>
+            </div>
           </div>
-          {/* text */}
-          <div className="flex flex-col gap-2">
-            <h2 className="font-normal text-[48px] text-[#C19F74] tracking-[0.02em] leading-[48px] ">
-              490+{" "}
-            </h2>
-            <h3 className="text-white text-[40px] font-normal tracking-[0.02em] leading-[40px] font-restora capitalize">
-              positive feedback
-            </h3>
-            <p className="text-white font-normal text-[16px] leading-[24px]">
-              Our restaurant offers a stylish and fun European cafe-restaurant
-              experience.
-            </p>
+          <div className="h-auto md:h-[714px] flex flex-col gap-[20px]">
+            <div className=" justify-center items-center">
+              <img
+                src={image1}
+                alt=""
+                loading="lazy"
+                className="object-cover w-full h-full"
+              />
+            </div>
+            {/* text */}
+            <div className="flex flex-col gap-2">
+              <h2 className="font-normal text-[48px] text-[#C19F74] tracking-[0.02em] leading-[48px] ">
+                1590+{" "}
+              </h2>
+              <h3 className="text-white text-[40px] font-normal tracking-[0.02em] leading-[40px] font-restora capitalize">
+                delivery order{" "}
+              </h3>
+              <p className="text-white font-normal text-[16px] leading-[24px]">
+                Our restaurant offers a stylish and fun European cafe-restaurant
+                experience.
+              </p>
+            </div>
           </div>
-        </div>
-        <div className="h-auto md:h-[714px] flex flex-col gap-[20px]">
-          <div className=" justify-center items-center">
-            <img
-              src={image1}
-              alt=""
-              loading="lazy"
-              className="object-cover w-full h-full"
-            />
-          </div>
-          {/* text */}
-          <div className="flex flex-col gap-2">
-            <h2 className="font-normal text-[48px] text-[#C19F74] tracking-[0.02em] leading-[48px] ">
-              1590+{" "}
-            </h2>
-            <h3 className="text-white text-[40px] font-normal tracking-[0.02em] leading-[40px] font-restora capitalize">
-              delivery order{" "}
-            </h3>
-            <p className="text-white font-normal text-[16px] leading-[24px]">
-              Our restaurant offers a stylish and fun European cafe-restaurant
-              experience.
-            </p>
-          </div>
-        </div>
-        <div className="h-auto md:h-[714px] flex flex-col gap-[20px]">
-        <div className="w-auto md:w-[438px] h-[518px]">
-        <img
-              src={image2}
-              alt=""
-              loading="lazy"
-              className="object-cover w-full h-full"
-            />
-          </div>
-          {/* text */}
-          <div className="flex flex-col gap-2">
-            <h2 className="font-normal text-[48px] text-[#C19F74] tracking-[0.02em] leading-[48px] ">
-              100%
-            </h2>
-            <h3 className="text-white text-[40px] font-normal tracking-[0.02em] leading-[40px] font-restora capitalize">
-              visitor Daily{" "}
-            </h3>
-            <p className="text-white font-normal text-[16px] leading-[24px]">
-              Our restaurant offers a stylish and fun European cafe-restaurant
-              experience.
-            </p>
+          <div className="h-auto md:h-[714px] flex flex-col gap-[20px]">
+            <div className="w-auto md:w-[438px] h-[518px]">
+              <img
+                src={image2}
+                alt=""
+                loading="lazy"
+                className="object-cover w-full h-full"
+              />
+            </div>
+            {/* text */}
+            <div className="flex flex-col gap-2">
+              <h2 className="font-normal text-[48px] text-[#C19F74] tracking-[0.02em] leading-[48px] ">
+                100%
+              </h2>
+              <h3 className="text-white text-[40px] font-normal tracking-[0.02em] leading-[40px] font-restora capitalize">
+                visitor Daily{" "}
+              </h3>
+              <p className="text-white font-normal text-[16px] leading-[24px]">
+                Our restaurant offers a stylish and fun European cafe-restaurant
+                experience.
+              </p>
+            </div>
           </div>
         </div>
       </div>
