4f997b14656e5dc4abee356721ddfc450133802a
Safeya-Yasien <ssafeya5@gmail.com>
2025-02-02 19:10:50 +0200

    build(deps): update package-lock.json, package.json and VerificationCode.tsx

--- src/pages/VerificationCode.tsx
+++ src/pages/VerificationCode.tsx
@@ -4,29 +4,44 @@ import { AuthBanner, WelcomeMessageWidget } from "@/components";
 
 import registerImg from "../assets/images/register.webp";
 import { GoArrowLeft } from "react-icons/go";
+import {
+  InputOTP,
+  InputOTPGroup,
+  InputOTPSlot,
+} from "@/components/ui/input-otp";
 
 const VerificationCode = () => {
   return (
     <div className="container relative z-50 min-h-screen flex items-center">
       <div className="grid grid-cols-1 md:grid-cols-2 w-full gap-10 md:gap-12 lg:gap-16 ">
         {/* left side */}
-        <div className="flex flex-col justify-center bg-[#131313] text-white">
+        <div className="flex flex-col justify-center bg-[#131313] text-white mx-auto">
           <WelcomeMessageWidget
             title="verification code"
             description="For security reasons, this code expires in 2 minutes."
           />
 
-          <form className="flex flex-col gap-6 ">
-            <div className="flex flex-col gap-[10px] ">
-              <label className="font-light text-[16px] capitalize ">
-                email
-              </label>
-              <input
-                type="email"
-                placeholder="Enter your email"
-                className="border border-[#C19F74] p-[10px] bg-transparent text-white placeholder-[#6F6F6F] outline-none"
-              />
-            </div>
+          <form className="flex flex-col gap-[40px] ">
+            <InputOTP maxLength={6} className="">
+              <InputOTPGroup className="flex gap-3">
+                <InputOTPSlot
+                  className="border border-[#C19F74] w-[125px] h-[100px] bg-transparent  text-white font-bold text-[56px] text-center !rounded-none"
+                  index={0}
+                />
+                <InputOTPSlot
+                  className="border border-[#C19F74] w-[125px] h-[100px] bg-transparent  text-white font-bold text-[56px] text-center !rounded-none"
+                  index={1}
+                />
+                <InputOTPSlot
+                  className="border border-[#C19F74] w-[125px] h-[100px] bg-transparent  text-white font-bold text-[56px] text-center !rounded-none"
+                  index={2}
+                />
+                <InputOTPSlot
+                  className="border border-[#C19F74] w-[125px] h-[100px] bg-transparent  text-white font-bold text-[56px] text-center !rounded-none"
+                  index={3}
+                />
+              </InputOTPGroup>
+            </InputOTP>
 
             <button className="bg-[#ECCBA2] text-black font-medium text-[20px]  flex items-center justify-center h-[58px]">
               Continue
